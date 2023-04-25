7da7613b4cc21e0e92c5e454f4fb5774146b8c16
tomduese <thomas.lettau@live.de>
2023-04-25 10:53:47 +0200

    feature engeneering

--- models/eda.py
+++ models/eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from imblearn.over_sampling import SMOTE
 
 
 def load_data(path):
@@ -76,4 +77,49 @@ def feature_change(cl, inv):
     df = pd.merge(cl, inv, on="client_id", how="left")
     df["client_id"] = df["client_id"].str.slice(start=13).astype(int)
 
+    return df
+
+
+def get_mean_consumption(df):
+    # Calculate the mean consumption per year for each client
+    mean_consumption = (
+        df.groupby(["client_id", "invoice_year"])["total_consumption"]
+        .mean()
+        .reset_index()
+    )
+
+    # Merge the mean consumption dataframe back into the original dfframe
+    df = df.merge(mean_consumption, on=["client_id", "invoice_year"], how="left")
+    df.rename(
+        columns={
+            "total_consumption_y": "mean_consumption_per_year",
+            "total_consumption_x": "total_consumption",
+        },
+        inplace=True,
+    )
+    return df
+
+
+# Define a function to calculate the historical mean consumption for each client
+def historical_mean(group):
+    # Sort the group by invoice year in ascending order
+    group = group.sort_values("invoice_year")
+
+    # Calculate the expanding mean for the total consumption column
+    group["historical_mean_consumption"] = group["total_consumption"].expanding().mean()
+
+    return group
+
+
+def get_historical_mean(df):
+    # Apply the function to the dataframe grouped by client id
+    df = df.groupby("client_id").apply(historical_mean)
+    df = df.reset_index(drop=True)
+    # Shift the historical mean consumption column by one row to avoid using the current year's mean
+    df["historical_mean_consumption"] = df.groupby("client_id")[
+        "historical_mean_consumption"
+    ].shift()
+    df.historical_mean_consumption = df.historical_mean_consumption.fillna(0)
+
+    # Check the updated dfframe
     return df
